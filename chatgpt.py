6181abacd8049f22055b9f5f480c25f954db6eb3
SungeunAmberKim <sungeunkim@cpp.edu>
2024-04-29 16:01:31 -0700

    tested 100 and 1000 iterations

--- chatgpt.py
+++ chatgpt.py
@@ -19,7 +19,19 @@ def generate_input_file(size: int, answer_form, filename):
             moe_steal = random.randrange(0,100, 5)
             correct_answer = ((emily_invested*uni_steal)/100)*(1+(moe_steal/100))
             file.write(f"{emily_invested},{emily_interst_1},{emily_interst_2},{uni_steal},{mom_invested},{mom_interest},{moe_steal},{answer_form},{correct_answer}\n")
-
+# generating input csv file; correct_answer is always 55   
+def generate_input_file_1(size: int, answer_form, filename):
+    with open(filename,"w") as file:
+        for i in range(size):
+            emily_invested = 5000
+            emily_interst_1 = random.randrange(1, 10, 1)
+            emily_interst_2 = random.randrange(1, 10, 1)
+            uni_steal = 1
+            mom_invested = random.randrange(1000,10000, 10)
+            mom_interest = random.randrange(1, 10, 1)
+            moe_steal = 10
+            correct_answer = 55
+            file.write(f"{emily_invested},{emily_interst_1},{emily_interst_2},{uni_steal},{mom_invested},{mom_interest},{moe_steal},{answer_form},{correct_answer}\n")
 # function to generate prompts from csv file
 def generate_prompts(infile,outfile):
     output = []
@@ -114,13 +126,16 @@ if __name__ == "__main__":
     input_csv = "input_1.csv"
     ouput_csv = "output_1.csv"
     clean_csv = "clean_1.csv"
-    generate_input_file(10, "one word", input_csv)
+    generate_input_file_1(1000, "one word", input_csv)
     prompts = generate_prompts(input_csv,ouput_csv)
     generate_output_file(prompts, input_csv, ouput_csv)
     data_cleaning(ouput_csv, clean_csv)
     result_1 = correctness(clean_csv)
-    with open('result.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(result_1)
+    with open('result.txt', 'a') as file:
+        file.write("input_1.csv, output_1.csv, clean_1.csv; one word; 1000 iterations\n")
+        file.write("temperature = 0.6; top_p = 1; frequency_penalty = 0; presence_penalty = 0\n")
+        file.write(f"{result_1[0]} - {result_1[1]}\n")
+        file.write("___________________________________________________________________________")
+    
         
     
